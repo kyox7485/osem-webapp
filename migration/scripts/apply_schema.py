b2f3127c33c067f110263831e40827a40d22670f
OSEM Dev <chaos7485@gmail.com>
2026-09-15 13:18:49 +0800

    Add schema-apply utility script

---
+++ migration/scripts/apply_schema.py
@@ -0,0 +1,18 @@
+﻿import os
+from dotenv import load_dotenv
+load_dotenv()
+import psycopg2
+dsn = os.environ["PG_DSN"]
+with open(r"C:\Users\NGF\dev\osem-webapp\schema\001_init.sql", "r", encoding="utf-8") as f:
+    sql = f.read()
+conn = psycopg2.connect(dsn, connect_timeout=10)
+conn.autocommit = False
+cur = conn.cursor()
+try:
+    cur.execute(sql)
+    conn.commit()
+    print("SCHEMA APPLIED OK")
+except Exception as e:
+    conn.rollback()
+    print("ERROR:", e)
+conn.close()
